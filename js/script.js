8b932bc01133b415783d50ffa52cbddc49efc4cf
Sara <saracorsob@gmail.com>
2020-06-25 15:48:16 +0200

    Struttura html

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-alert('ciao');
+// Griglia 6x6, ad ogni click parte una richiesta AJAX che prende un numero random
+// da 1 a 9.
+// Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
+// Il numero ottenuto appare al centro del quadrato
+$(document).ready(function() {
+
+
+
+});
